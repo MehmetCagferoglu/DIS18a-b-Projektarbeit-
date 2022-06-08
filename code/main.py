f8b02600657c271f996fa711068a63510b4c67ec
FurkanERD <furkan.erdogan@smail.th-koeln.de>
2022-06-08 16:07:31 +0200

    Updated completly main.py
    Es wurden überall Kommentare hinzugefügt zur sachlichen Verständigung.
    -Das Ganze wurde blackified "Black".
    
    Co-Authored-By: Markus854 <63366701+Markus854@users.noreply.github.com>
    Co-Authored-By: ToKbArRy <63405924+ToKbArRy@users.noreply.github.com>

--- code/main.py
+++ code/main.py
@@ -1,129 +1,119 @@
-# Importieren wichtiger Libraries
+# Importieren der Libraries.
 import re
 from pathlib import Path
-import json 
+import json
 
+# Mithilfe der Pathlib-Library der aktuellen Workingdirectory finden und zwei Ordnerpfade zurückgehen.
 cwd = Path(__file__).resolve().parent.parent
 
-with open(str(cwd)+"\data/index_of_works_v1-7", "r",encoding='utf-8') as a:
-     RawText = a.read()
-
-# Ersteller der Werke
-Creator = re.findall('creator">([^"]*?)<',RawText) 
+# Temporäres Öffnen der Datei mit den Rohdaten.
+with open(str(cwd) + "\data/index_of_works_v1-7", "r", encoding="utf-8") as a:
+    RawText = a.read()
+# Regexabfrage zum Auslesen des Erstellers der Werke.
+Creator = re.findall('creator">([^"]*?)<', RawText)
 
 for x in range(len(Creator)):
     if Creator[x] == "Fantin":
         Creator[x] = "Henri Fantin-Latour"
     else:
         Creator[x] = "Otto Scholderer"
-
-# Titel in verschiedenen Sprachen
+# Leere Listen für die Titel in verschiedenen Sprachen erzeugen.
 titel_de = []
 titel_fr = []
 titel_en = []
 
-# Eine Liste, die alle Titeln heraussucht
-Name = re.findall('name">([^"]*?)<',RawText)
+# Regexabfrage, die alle Titel in einer Liste heraussucht.
+Name = re.findall('name">([^"]*?)<', RawText)
 
-# Definition von a und b mit Regex. Für a = "\(" und für b = "\/"
-a = re.compile(r'\(')
-b = re.compile(r'\/')
+# Definition von a und b mit Regex. Für a = "\(" und für b = "\/".
+a = re.compile(r"\(")
+b = re.compile(r"\/")
 
 
-# Funktion für alle Titeln (titel all)
 for x in range(len(Name)):
+    # Wenn eine Klammer und ein Slash enthalten sind, gibt es Titel in drei Sprachen (titel_de, titel_en und titel_fr).
     if a.search(Name[x]) and b.search(Name[x]):
-        en =  re.findall('.*\/',Name[x])
+        en = re.findall(".*\/", Name[x])
         for y in en:
             titel_en.append(y[:-3])
-        de =  re.findall('\/.*\/|\/.*\(',Name[x])
+        de = re.findall("\/.*\/|\/.*\(", Name[x])
         for y in de:
             titel_de.append(y[2:-2])
-        fr =  re.findall('\(.*?\)',Name[x])
+        fr = re.findall("\(.*?\)", Name[x])
         for y in fr:
             titel_fr.append(y[2:-2])
-      
-# Funktion nur für das französische Titel (titel_fr)
+    # Wenn nichts enthalten ist, ist der Titel französisch (titel_fr).
     if not a.search(Name[x]) and not b.search(Name[x]):
         titel_fr.append(Name[x])
         titel_en.append("")
         titel_de.append("")
-    
-# Funktion für das französische und deutsche Titel (titel_de und titel_fr)         
+    # Wenn eine Klammer und kein Slash enthalten ist, dann ist der Titel französisch und deutsch (titel_de und titel_fr).
     if a.search(Name[x]) and not b.search(Name[x]):
-         titel_fr.append(str(re.findall('\(.*?\)', Name[x])[-1])[1:-1])
-         titel_de.append(str(re.findall('.*\(', Name[x])[-1])[:-2])
-         titel_en.append("")
-
-       
-# Maße (Länge und Breite) und die Funktion dafür     
-Maße = re.findall('>([^>]*?cm)<|>0<', RawText)
-länge  = []
+        titel_fr.append(str(re.findall("\(.*?\)", Name[x])[-1])[1:-1])
+        titel_de.append(str(re.findall(".*\(", Name[x])[-1])[:-2])
+        titel_en.append("")
+# Regexabfrage zum Finden der Maße (Länge und Breite) und Erstellung der leeren Listen zur Speicherung der Werte.
+Maße = re.findall(">([^>]*?cm)<|>0<", RawText)
+länge = []
 breite = []
 
+# Regexabfrage, um Länge und Breite der Maße vorher zu extrahieren. Zudem wird geprüft, ob überhaupt Maße enthalten sind.
 for x in range(len(Maße)):
     if len(Maße[x]) > 3:
-        a = re.findall('([0-9,]+)', Maße[x])
-        länge.append(a[0]+"cm") 
-        breite.append(a[1]+"cm")
+        a = re.findall("([0-9,]+)", Maße[x])
+        länge.append(a[0] + "cm")
+        breite.append(a[1] + "cm")
     if len(Maße[x]) < 3:
-    
-        länge.append("") 
+
+        länge.append("")
         breite.append("")
+# Regexabfrage zum Auslesen des Erstellungsjahres
+Release = re.findall('dateCreated">([^"]*?)<', RawText)
 
-
-# Erstellungsjahr
-Release = re.findall('dateCreated">([^"]*?)<',RawText)
-   
-# Speicherung in einem Dictionary
+# Erstellung des Dictionarys zur Speicherung.
 Dict = {}
 
-
+# Eintragen der extrahierten Daten in das Dictionary.
 for x in range(len(Creator)):
-    y = x + 1 
-# Funktion des Erstellers (Creator)   
-    Dict[y] = {     "Creator" : Creator[x],
-                    "titel" : {"titel_de" : "null",
-                               "titel_de" : "null",
-                               "titel_en" : "null"
-                                            }
-              }      
-# Deutscher Titel   
+    y = x + 1
+
+    # Funktion des Erstellers (Creator)
+    Dict[y] = {
+        "Creator": Creator[x],
+        "titel": {"titel_de": "null", "titel_de": "null", "titel_en": "null"},
+    }
+
+    # Befüllen der Titel.
+    # Deutscher Titel
     if len(titel_de[x]) > 3:
         Dict[y]["titel"]["titel_de"] = titel_de[x]
     if len(titel_de[x]) < 3:
-            Dict[y]["titel"]["titel_de"] = titel_fr[x]
-# Französischer Titel
+        Dict[y]["titel"]["titel_de"] = titel_fr[x]
+    # Französischer Titel
     if len(titel_fr[x]) > 3:
         Dict[y]["titel"]["titel_fr"] = titel_fr[x]
     if len(titel_fr[x]) < 3:
-        Dict[y]["titel"]["titel_fr"] = titel_de[x]         
-# Englischer Titel
+        Dict[y]["titel"]["titel_fr"] = titel_de[x]
+    # Englischer Titel
     if len(titel_en[x]) > 3:
-       Dict[y]["titel"]["titel_en"] = titel_en[x] 
+        Dict[y]["titel"]["titel_en"] = titel_en[x]
     if len(titel_en[x]) < 3:
         if Creator[x] == "Henri Fantin-Latour":
-            Dict[y]["titel"]["titel_en"] = titel_fr[x] 
-            if  titel_fr[x] == "":
+            Dict[y]["titel"]["titel_en"] = titel_fr[x]
+            if titel_fr[x] == "":
                 Dict[y]["titel"]["titel_en"] = titel_de[x]
         if Creator[x] == "Otto Scholderer":
             Dict[y]["titel"]["titel_en"] = titel_de[x]
-            if  titel_de[x] == "":
+            if titel_de[x] == "":
                 Dict[y]["titel"]["titel_en"] = titel_fr[x]
-# Release           
+    # Release
     Dict[y]["Release"] = Release[x]
-# Maße   
+
+    # Maße
     Dict[y]["Maße"] = Maße[x]
-
-cwd2 = Path(__file__).resolve().parent.parent    
+# Speichern des Dictionarys im Ordner "data".
+cwd2 = Path(__file__).resolve().parent.parent
 with open(str(cwd2) + "\data\Data.json", "w") as outfile:
+
+    # Umwandlen des Dictionarys in json.
     json.dump(Dict, outfile)
-
-
-"""
-    
-j = ""
-for x in range(1,368):
-    j = j + str(Dict[x]["titel"]) + "\n"
-        
-"""
